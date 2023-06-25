42ac223b3284d2df4cfb5e82856783355af3fe8b
Himanshu Sharma <himanshu.sharma@spenda.co>
2023-06-25 21:00:53 +0530

    finish

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './brand.css';
+import {google ,atlassian ,shopify , slack,dropbox} from './import.js'
 
 const Brand = () => {
   return (
-    <div>
-      Brand
+    <div className="gpt3__brand section__padding">
+      <div >
+        <img src={google} alt='google'/>
+      </div>
+      <div >
+        <img src={atlassian} alt='atlassian'/>
+      </div>
+      <div >
+        <img src={slack} alt='slack'/>
+      </div>
+      <div >
+        <img src={shopify} alt='shopify'/>
+      </div>
+      <div >
+        <img src={dropbox} alt='dropbox'/>
+      </div>
     </div>
   )
 }
