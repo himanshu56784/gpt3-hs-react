3048752ba7b64c069a5def6e176a4f3d7bf6ab15
Himanshu Sharma <himanshu56784@gmail.com>
2023-06-25 09:23:07 +0530

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Brand,CTA,Navbar} from './components';
+import { WhatGPT3,Possibility,Header,Footer,Features,Blog } from './container';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="App"> 
+      <div className="gradient__bg">
+        <Navbar></Navbar>
+        <Header></Header>
+      </div>
+      
+        <Brand></Brand>
+        <WhatGPT3></WhatGPT3>
+        <Features></Features>
+        <Possibility></Possibility>
+        <CTA></CTA>
+        <Blog></Blog>
+        <Footer></Footer>
+      
+      </div>
+  )
 }
-
 export default App;
